7f74289e301e57862e3fe8ac580355bd77759900
Dariya <dkladova@list.ru>
2022-07-10 20:58:22 +0300

    Added task0

---
+++ task0/Program.cs
@@ -0,0 +1,6 @@
+﻿//найти квадрат числа, вывести на экран
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number*number;
+Console.WriteLine(square);
