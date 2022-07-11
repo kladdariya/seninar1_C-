0b49382dd9b7795f3de4f06f47328fa6303902cd
Dariya <dkladova@list.ru>
2022-07-11 12:55:11 +0300

    create progect HW4

---
+++ HW4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+
+Console.WriteLine (max);
