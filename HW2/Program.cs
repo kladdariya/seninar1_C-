45bed14c29800dc8c60cfcd727d1bd1b2a34279f
Dariya <dkladova@list.ru>
2022-07-11 12:37:19 +0300

    create code

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,4 +1,5 @@
 Console.WriteLine("Введите число: ");
+
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
@@ -6,15 +7,16 @@ int min = number1;
 
 if ( number1 > max ) max = number1;
 if ( number2 > max ) max = number2;
+if ( number1 < min ) min = number1;
+if ( number2 < min ) min = number2;
 
 {
-    Console.Write("max = ");
-    Console.WriteLine(max);
-}
+    
+    Console.Write ("max = ");
+    Console.WriteLine (max);
 
-else
-{
-    Console.Write("min = ");
-    Console.WriteLine(min);
+    Console.Write ("min = ");
+    Console.WriteLine (min);
+
 }
 
