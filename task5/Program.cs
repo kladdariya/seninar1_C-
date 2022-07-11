04f8742fdc1f2dc069afdf03443d02cd35983039
Dariya <dkladova@list.ru>
2022-07-11 14:41:16 +0300

    create progect task5

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+while(count<=N)
+{
+    Console.WriteLine(count);
+    count++; 
+
+}
